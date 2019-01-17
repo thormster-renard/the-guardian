87541824d1aa22959f866a11cf8cafd89142e463
thormster-renard <thormster-renard>
2019-01-17 01:03:47 +0100

    Portabilité améliorée GNU 	modifié :         sources/PersonnageSelectorState.cpp

--- sources/PersonnageSelectorState.cpp
+++ sources/PersonnageSelectorState.cpp
@@ -20,76 +20,74 @@ PersonnageSelectorState::~PersonnageSelectorState()
 
 void    PersonnageSelectorState::printMenu()
 {
-    int i;
+  int i;
 
-    i = 0;
-        #ifdef _WIN32
-            system("CLS");
-        #elif __linux__
-            system("clear");
-        #endif
-    std::cout
-        << gui::msgMenuTitre("Selection du personnage")
-        << gui::msgMenuDiviseur(40, '-')
-        << gui::msgMenuItem(10, 1, "Menu precedent")
-        << gui::msgMenuDiviseur(40, '-');
-    if (!this->listePersonnage->empty())
+  i = 0;
+#ifdef _WIN32
+  system("CLS");
+#elif __linux__
+  system("clear");
+#endif
+  std::cout
+    << gui::msgMenuTitre("Selection du personnage")
+    << gui::msgMenuDiviseur(40, '-')
+    << gui::msgMenuItem(10, 1, "Menu precedent")
+    << gui::msgMenuDiviseur(40, '-');
+  if (!this->listePersonnage->empty())
     {
-        while (i < this->listePersonnage->size())
+      while (i < this->listePersonnage->size())
         {
-            std::cout << " (" << i << ") " << this->listePersonnage->at(i)->getMenuBar(false) << std::endl;
-            i += 1;
+	  std::cout << " (" << i << ") " << this->listePersonnage->at(i)->getMenuBar(false) << std::endl;
+	  i += 1;
         }
-        std::cout << "\n";
-    } 
+      std::cout << "\n";
+    }
 }
 
 void    PersonnageSelectorState::updateMenu()
 {
-    int choice;
+  int choice;
 
-    if (!this->listePersonnage->empty())
+  if (!this->listePersonnage->empty())
     {
-        choice = this->getChoice();
-        if (choice < 0)
+      choice = this->getChoice();
+      if (choice < 0)
+	this->setQuit(true);
+      else if (choice >= 0 && choice < this->listePersonnage->size())
         {
-            this->setQuit(true);
+	  this->personnageActif = choice;
+	  this->setQuit(true);
+	  std::cout << "Personnage " << choice << " selectionne." << std::endl;
+#ifdef _WIN32
+	  system("PAUSE");
+#elif __linux__
+	  std::cin.get();
+#endif
         }
-        else if (choice >= 0 && choice < this->listePersonnage->size())
+      else
         {
-            this->personnageActif = choice;
-            this->setQuit(true);
-            std::cout << "Personnage " << choice << " selectionne." << std::endl;
-             #ifdef _WIN32
-            system("PAUSE");
-        #elif __linux__
-            std::cin.get();
-        #endif
-        }
-        else
-        {
-            std::cout << gui::msgErreur("Hum, ce personnage n'existe pas.");
-           #ifdef _WIN32
-            system("PAUSE");
-        #elif __linux__
-            std::cin.get();
-        #endif
+	  std::cout << gui::msgErreur("Hum, ce personnage n'existe pas.");
+#ifdef _WIN32
+	  system("PAUSE");
+#elif __linux__
+	  std::cin.get();
+#endif
         }
     }
-    else
+  else
     {
-        this->setQuit(true);
-        std::cout << "Aucun personnage n'a encore ete cree." << std::endl;
-          #ifdef _WIN32
-            system("PAUSE");
-        #elif __linux__
-            std::cin.get();
-        #endif
+      this->setQuit(true);
+      std::cout << "Aucun personnage n'a encore ete cree." << std::endl;
+#ifdef _WIN32
+      system("PAUSE");
+#elif __linux__
+      std::cin.get();
+#endif
     }  
 }
 
 void        PersonnageSelectorState::update()
 {
-     this->printMenu();
-     this->updateMenu();
+  this->printMenu();
+  this->updateMenu();
 }
